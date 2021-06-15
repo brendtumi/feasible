36544214d57a4ea689cf15b3b98de16b5c8ba280
Tümay Çeber <tumayceber@gmail.com>
2021-06-15 19:14:56 +0300

    feat: remote config files

--- bin/feasible.js
+++ bin/feasible.js
@@ -16,12 +16,16 @@ program
   //.command('init', {isDefault: true})
   .description('Start setup process')
   .option('-c, --config <FilePath>', 'Configuration file', 'feasible.{json,json5,yml}')
+  .option(
+    '-u, --url <FileUrl>',
+    'Configuration file url. Example: https://my-private-repo.git.com/raw/dope-repository/main/feasible.{json,json5,yml}?token=TOKEN',
+  )
   .option('-f, --force', 'Overwrite current setup if it exists and start over', false)
   .option('-p, --parallel', 'Enable parallel actions if possible', false)
   .option('-s, --separator <Separator>', 'Default separator for variable and values', '=')
   .option('-n, --no-clean', "Don't clean up old output files")
-  .action(({ config, force, parallel, noClean }) => {
-    require('../lib')({ config, force, parallel, noClean });
+  .action(({ config, url, force, parallel, noClean }) => {
+    require('../lib')({ config, url, force, parallel, noClean });
   });
 
 program.parse(process.argv);
